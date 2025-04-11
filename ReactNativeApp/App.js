6af601d25840b78f5368a3215d11e76917294462
Aaryan Tomar <140002718+Aaryan12082003@users.noreply.github.com>
2025-04-11 20:28:41 +0530

    Add ReactNativeApp/App.js 1744383522133

---
+++ ReactNativeApp/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import WelcomeScreen from './WelcomeScreen';
+import SignupScreen from './SignupScreen';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
+        <Stack.Screen name="Signup" component={SignupScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
